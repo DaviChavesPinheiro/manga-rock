99d4e41ef4be7a2fe8684d89ab7c5497dbe3152c
Davi <davicpinheiro123@hotmail.com>
2020-04-03 11:36:44 -0300

    Adicionei alguns themas, mas nao sei como usar eles ainda

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MangaRock',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(35, 35, 35, 1),
+        accentColor: Colors.blue,
+        canvasColor: Colors.black,
+        textTheme: ThemeData().textTheme.copyWith(
+              headline: TextStyle(
+                fontSize: 17,
+                color: Colors.white,
+              ),
+              body1: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              subhead: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -14,7 +32,13 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child: Text('Home'),
+          child: Text(
+            'Home',
+            // style: TextStyle(
+            //   fontSize: Theme.of(context).textTheme.headline.fontSize,
+            //   color: Colors.white
+            // ),
+          ),
         ),
       ),
     );
