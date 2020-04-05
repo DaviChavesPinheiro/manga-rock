0ba6dc27c6fb6f8ee1ece9b716f0cb5558afccee
Davi <davicpinheiro123@hotmail.com>
2020-04-05 10:07:00 -0300

    MangProfile: Criei a classe do manga Profile

---
+++ lib/models/manga_profile.dart
@@ -0,0 +1,8 @@
+class MangaProfile {
+  final String bannerUrl;
+  final String thumbnailUrl;
+  final String title;
+  final String description;
+
+  const MangaProfile({this.bannerUrl, this.thumbnailUrl, this.title, this.description = "Descrição"});
+}
