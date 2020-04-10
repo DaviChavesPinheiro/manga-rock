ecaef02573d4d34c647126196f774af3d920c06a
Davi <davicpinheiro123@hotmail.com>
2020-04-10 13:57:17 -0300

    HorizontalList: Agr o horizontalList recebe dados do firestore

--- lib/screens/forYou_screen.dart
+++ lib/screens/forYou_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:manga_rock/components/horizontal_manga_list.dart';
-import 'package:manga_rock/data/DATA.dart';
 import 'package:manga_rock/models/manga_profile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ForYouScreen extends StatelessWidget {
+  MangaProfile cloudDataToMangaProfile(DocumentSnapshot document) {
+    return MangaProfile(
+      title: document['title'],
+      autor: document['autor'],
+      description: document['description'],
+      thumbnailUrl: document['thumbnailUrl'],
+      bannerUrl: document['bannerUrl'],
+      chapters: List<String>.from(document['chapters']),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -14,21 +24,18 @@ class ForYouScreen extends StatelessWidget {
             ? ListView(
                 padding: EdgeInsets.fromLTRB(15, 30, 0, 30),
                 children: <Widget>[
-                  HorizontalMangaList(
-                    [
-                      MangaProfile(
-                        title: snapshot.data.documents[0]['title'],
-                        autor: snapshot.data.documents[0]['autor'],
-                        description: snapshot.data.documents[0]['description'],
-                        thumbnailUrl: snapshot.data.documents[0]['thumbnailUrl'],
-                        bannerUrl: snapshot.data.documents[0]['bannerUrl'],
-                        chapters: List<String>.from(snapshot.data.documents[0]['chapters']),
-                      ),
-                    ],
-                  ),
-                  HorizontalMangaList(MANGAS),
-                  HorizontalMangaList(MANGAS),
-                  HorizontalMangaList(MANGAS),
+                  HorizontalMangaList(List<MangaProfile>.from(snapshot.data.documents.map((document){
+                    return cloudDataToMangaProfile(document);
+                  }))),
+                  HorizontalMangaList(List<MangaProfile>.from(snapshot.data.documents.map((document){
+                    return cloudDataToMangaProfile(document);
+                  }))),
+                  HorizontalMangaList(List<MangaProfile>.from(snapshot.data.documents.map((document){
+                    return cloudDataToMangaProfile(document);
+                  }))),
+                  HorizontalMangaList(List<MangaProfile>.from(snapshot.data.documents.map((document){
+                    return cloudDataToMangaProfile(document);
+                  }))),
                 ],
               )
             : Text("Mangás Indisponíveis :(");
