f7f17d1fb5d47a023306faa2ca2b7a28d545180c
Davi <davicpinheiro123@hotmail.com>
2020-04-14 16:53:22 -0300

    All_Screen: Implement fireStore Data into AllScreen

--- lib/screens/all_screen.dart
+++ lib/screens/all_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:manga_rock/components/manga_tile.dart';
+import 'package:manga_rock/models/manga_profile.dart';
 
 class AllScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("All"),
-    );
+    return StreamBuilder(
+        stream: Firestore.instance.collection("mangas").snapshots(),
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? GridView.builder(
+                  itemCount: snapshot.data.documents.length, //TIRAR ISSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+                  padding: const EdgeInsets.all(8),
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: MediaQuery.of(context).size.width /
+                          (MediaQuery.of(context).orientation ==
+                                  Orientation.portrait
+                              ? 3
+                              : 6),
+                      childAspectRatio: 1 / 1.8,
+                      crossAxisSpacing: 2,
+                      mainAxisSpacing: 2),
+                  itemBuilder: (ctx, index) {
+                    return MangaTile(
+                      MangaProfile.fromDocumentSnapshot(snapshot.data.documents[index])
+                    );
+                  },
+                )
+              : Text("Carregando Favoritos...");
+        });
   }
 }
--- lib/screens/manga_profile_screen.dart
+++ lib/screens/manga_profile_screen.dart
@@ -6,64 +6,6 @@ import 'package:manga_rock/models/manga_profile.dart';
 
 class MangaProfileScreen extends StatelessWidget {
 
-  void viewManga(BuildContext context, mangaProfile){
-    // Navigator.of(context).pushNamed(
-    //   AppRoutes.MANGA_VIEW,
-    //   arguments: mangaProfile
-    // );
-  }
-
-  // final paginas = const [
-  //   "http://unionleitor.top/images/banner_scan.png",
-  //   "http://unionleitor.top/images/banner_forum.png",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/000.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/001.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/002.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/003.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/004.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/005.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/006.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/007.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/008.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/009.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/010.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/011.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/012.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/013.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/014.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/015.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/016.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/017.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/018.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/019.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/020.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/021.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/022.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/023.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/024.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/025.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/026.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/027.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/028.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/029.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/030.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/031.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/032.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/033.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/034.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/035.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/036.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/037.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/038.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/039.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/040.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/041.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/042.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/043.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/044.jpg",
-  //   "http://unionleitor.top/leitor/mangas/Tokyo Ghoul/01/045.jpg"
-  // ];
-
   @override
   Widget build(BuildContext context) {
     final MangaProfile mangaProfile = ModalRoute.of(context).settings.arguments;
